dbc0016612e134e673e81c733bc242f5a562f670
unly <leongrch@gmail.com>
2020-09-08 12:08:19 +0200

    adding some docs (#5)

--- client.go
+++ client.go
@@ -9,13 +9,18 @@ import (
 
 const baseUrl = "https://www.tukui.org/api.php"
 
+// The Client is a simple http client to access the TukUI.org API.
+// Addons can be accessed either by the RetailAddons or ClassicAddons field.
 type Client struct {
 	url           string
 	httpClient    *http.Client
-	RetailAddons  *AddonClient
-	ClassicAddons *AddonClient
+	RetailAddons  *addonClient
+	ClassicAddons *addonClient
 }
 
+// NewClient creates a new Client struct and returns a pointer to it.
+// Optionnaly you can pass a pointer to a http.Client to be used. Alternatively,
+// http.DefaultClient is used.
 func NewClient(client *http.Client) *Client {
 	if client == nil {
 		client = http.DefaultClient
@@ -25,11 +30,11 @@ func NewClient(client *http.Client) *Client {
 		url:        baseUrl,
 		httpClient: client,
 	}
-	c.RetailAddons = &AddonClient{
+	c.RetailAddons = &addonClient{
 		client:  &c,
 		classic: false,
 	}
-	c.ClassicAddons = &AddonClient{
+	c.ClassicAddons = &addonClient{
 		client:  &c,
 		classic: true,
 	}
