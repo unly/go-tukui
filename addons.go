02aa4c8bba8c439cd796c765091ec4a2968d5def
unly <leongrch@gmail.com>
2020-09-08 12:06:23 +0200

    adding some docs

--- addons.go
+++ addons.go
@@ -5,36 +5,54 @@ import (
 	"strconv"
 )
 
+// An Addon is the basic return of the HTTP call. It contains the fields specified by
+// the API. Pointers to strings can be nil.
 type Addon struct {
-	Id            *string `json:"id,omitempty"`
-	Name          *string `json:"name,omitempty"`
-	SmallDesc     *string `json:"small_desc,omitempty"`
-	Author        *string `json:"author,omitempty"`
-	Version       *string `json:"version,omitempty"`
+	// ID number of addon
+	Id *string `json:"id,omitempty"`
+	// the title of addon
+	Name *string `json:"name,omitempty"`
+	// a condensed description of addon for small area
+	SmallDesc *string `json:"small_desc,omitempty"`
+	// the author username
+	Author *string `json:"author,omitempty"`
+	// latest version of the addon uploaded on our network
+	Version *string `json:"version,omitempty"`
+	// a screenshot url of the addon
 	ScreenshotUrl *string `json:"screenshot_url,omitempty"`
-	URL           *string `json:"url,omitempty"`
-	Category      *string `json:"category,omitempty"`
-	Downloads     *string `json:"downloads,omitempty"`
-	LastUpdate    *string `json:"lastupdate,omitempty"`
-	Patch         *string `json:"patch,omitempty"`
-	WebUrl        *string `json:"web_url,omitempty"`
-	LastDownload  *string `json:"last_download,omitempty"`
-	DonateUrl     *string `json:"donate_url,omitempty"`
+	// URL to download the .zip file
+	URL *string `json:"url,omitempty"`
+	// the main category where the addon is located
+	Category *string `json:"category,omitempty"`
+	// the total number of downloads of this addon
+	Downloads *string `json:"downloads,omitempty"`
+	// the last time the addon was updated
+	LastUpdate *string `json:"lastupdate,omitempty"`
+	// which World of Warcraft patch this addon is compatible with
+	Patch *string `json:"patch,omitempty"`
+	// url of the addon if an user want to visit his official download web page on our website
+	WebUrl *string `json:"web_url,omitempty"`
+	// when the addon was downloaded for the last time
+	LastDownload *string `json:"last_download,omitempty"`
+	// a donate url if the addon author accept donations
+	DonateUrl *string `json:"donate_url,omitempty"`
 }
 
-type AddonClient struct {
+type addonClient struct {
 	client  *Client
 	classic bool
 }
 
-func (c *AddonClient) getQueryParameter(s string) string {
+func (c *addonClient) getQueryParameter(s string) string {
 	if c.classic {
 		return "classic-" + s
 	}
 	return s
 }
 
-func (c *AddonClient) GetAddon(id int) (Addon, *http.Response, error) {
+// GetAddon returns the Addon for the given ID. The ID is a positive number.
+// For non existing IDs the function will return an error.
+func (c *addonClient) GetAddon(id int) (Addon, *http.Response, error) {
 	var addon Addon
 
 	req, err := http.NewRequest(http.MethodGet, c.client.url, nil)
@@ -51,7 +69,8 @@ func (c *AddonClient) GetAddon(id int) (Addon, *http.Response, error) {
 	return addon, resp, err
 }
 
-func (c *AddonClient) GetAddons() ([]Addon, *http.Response, error) {
+// GetAddons retuns a slice of all Addons available.
+func (c *addonClient) GetAddons() ([]Addon, *http.Response, error) {
 	var addons []Addon
 
 	req, err := http.NewRequest(http.MethodGet, c.client.url, nil)
